9f6606966be963e369b35efeb3a98a1becb9c46a
Anderson S. Andrade <asa.sousa@gmail.com>
2023-11-30 20:38:03 -0300

    Update - Battery monitoring

--- src/main.cpp
+++ src/main.cpp
@@ -175,267 +175,270 @@ void setup()
 
 void loop()
 {
-  /*
-  Forward------------------F
-
-  Back---------------------B
-
-  Left---------------------L
-
-  Right--------------------R
-
-  Forward Left-------------G
-
-  Forward Right------------I
-
-  Back Left----------------H
-
-  Back Right---------------J
-
-  Stop---------------------S
-
-  Front Lights On----------W
-
-  Front Lights Off---------w (lower case)
-
-  Back Lights On-----------U
-
-  Back Lights Off----------u (lower case)
-
-  Horn On------------------V
-
-  Horn Off-----------------v (lower case)
-
-  Extra On-----------------X
-
-  Extra Off----------------x (lower case)
-
-  Speed 0------------------0
-
-  Speed 10-----------------1
-
-  Speed 20-----------------2
-
-  Speed 30-----------------3
-
-  Speed 40-----------------4
-
-  Speed 50-----------------5
-
-  Speed 60-----------------6
-
-  Speed 70-----------------7
-
-  Speed 80-----------------8
-
-  Speed 90-----------------9
-
-  Speed 100----------------q
-
-  Everything OFF-----------D (Tudo desligado)
-  */
-
-  if (SerialBT.available())
-  {
-    command = SerialBT.read();
-
-    // Serial.print("command: ");
-    // Serial.println(command);
-  }
-
-  if (command != commandOld)
-  {
-    commandOld = command;
-
-    Serial.print("command: ");
-    Serial.println(command);
-
-    switch (command)
-    {
-    case 'F':
-    { // move frente
-      motorForward();
-      directionCenter();
-      break;
-    }
-    case 'I':
-    { // frente direita
-      motorForward();
-      directionRight();
-
-      Serial.println("Forward Right");
-      break;
-    }
-    case 'G':
-    { // frente esquerda
-      motorForward();
-      directionLeft();
-
-      Serial.println("Forward Left");
-      break;
-    }
-    case 'R':
-    { // direita
-      motorStop();
-      directionRight();
-      break;
-    }
-    case 'L':
-    { // esquerda
-      motorStop();
-      directionLeft();
-      break;
-    }
-    case 'B':
-    { // ré
-      motorBack();
-      directionCenter();
-      break;
-    }
-    case 'H':
-    { // ré esquerda
-      motorBack();
-      directionLeft();
-
-      Serial.println("Back Left");
-      break;
-    }
-    case 'J':
-    { // ré direita
-      motorBack();
-      directionRight();
-
-      Serial.println("Back Right");
-      break;
-    }
-    case 'W':
-      Serial.println("Front Lights");
-      break;
-    case 'w':
-      Serial.println("Front Lights Off");
-      break;
-    case 'U':
-      Serial.println("Back Lights On");
-      break;
-    case 'u':
-      Serial.println("Back Lights Off");
-      break;
-    case 'V':
-      play_notes = true;
-      xTaskCreate(taskPlayNotes, "Play Notes", 1024 * 4, NULL, 5, NULL);
-
-      Serial.println("Horn On");
-      break;
-    case 'v':
-      play_notes = false;
-
-      Serial.println("Horn Off");
-      break;
-    case 'X':
-      Serial.println("Extra On");
-      break;
-    case 'x':
-      Serial.println("Extra Off");
-      break;
-    case '0':
-    {
-      speed = 0;
-      Serial.printf("Speed 0: %i\n", speed);
-      break;
-    }
-    case '1':
-    {
-      speed = 120;
-      Serial.printf("Speed 1: %i\n", speed);
-      break;
-    }
-    case '2':
-    {
-      speed = 130;
-      Serial.printf("Speed 2: %i\n", speed);
-      break;
-    }
-    case '3':
-    {
-      speed = 140;
-      Serial.printf("Speed 3: %i\n", speed);
-      break;
-    }
-    case '4':
-    {
-      speed = 160;
-      Serial.printf("Speed 4: %i\n", speed);
-      break;
-    }
-    case '5':
-    {
-      speed = 180;
-      Serial.printf("Speed 5: %i\n", speed);
-      break;
-    }
-    case '6':
-    {
-      speed = 200;
-      Serial.printf("Speed 6: %i\n", speed);
-      break;
-    }
-    case '7':
-    {
-      speed = 210;
-      Serial.printf("Speed 7: %i\n", speed);
-      break;
-    }
-    case '8':
-    {
-      speed = 220;
-      Serial.printf("Speed 8: %i\n", speed);
-      break;
-    }
-    case '9':
-    {
-      speed = 240;
-      Serial.printf("Speed 9: %i\n", speed);
-      break;
-    }
-    case 'q':
-    {
-      speed = 255;
-      Serial.printf("Speed 10: %i\n", speed);
-      break;
-    }
-    case 'D':
-    { // desliga
-      status_connected = false;
-
-      Serial.println("Everything OFF");
-      break;
-    }
-    case 'S':
-    default:
-    {
-      status_connected = true;
-      digitalWrite(CONNECTED_PIN, LOW);
-      directionCenter();
-      motorStop();
-      break;
-    }
-    }
-  }
-
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillisStatus >= interval_status && !status_connected)
+  if (!battery_low)
   {
-    previousMillisStatus = currentMillis;
+    /*
+    Forward------------------F
 
-    digitalWrite(CONNECTED_PIN, !digitalRead(CONNECTED_PIN));
-  }
+    Back---------------------B
 
-  if (currentMillis - previousMillisBattery >= interval_battery_read && !battery_low)
-  {
-    previousMillisBattery = currentMillis;
+    Left---------------------L
 
-    // loopBattery();
+    Right--------------------R
+
+    Forward Left-------------G
+
+    Forward Right------------I
+
+    Back Left----------------H
+
+    Back Right---------------J
+
+    Stop---------------------S
+
+    Front Lights On----------W
+
+    Front Lights Off---------w (lower case)
+
+    Back Lights On-----------U
+
+    Back Lights Off----------u (lower case)
+
+    Horn On------------------V
+
+    Horn Off-----------------v (lower case)
+
+    Extra On-----------------X
+
+    Extra Off----------------x (lower case)
+
+    Speed 0------------------0
+
+    Speed 10-----------------1
+
+    Speed 20-----------------2
+
+    Speed 30-----------------3
+
+    Speed 40-----------------4
+
+    Speed 50-----------------5
+
+    Speed 60-----------------6
+
+    Speed 70-----------------7
+
+    Speed 80-----------------8
+
+    Speed 90-----------------9
+
+    Speed 100----------------q
+
+    Everything OFF-----------D (Tudo desligado)
+    */
+
+    if (SerialBT.available())
+    {
+      command = SerialBT.read();
+
+      // Serial.print("command: ");
+      // Serial.println(command);
+    }
+
+    if (command != commandOld)
+    {
+      commandOld = command;
+
+      Serial.print("command: ");
+      Serial.println(command);
+
+      switch (command)
+      {
+      case 'F':
+      { // move frente
+        motorForward();
+        directionCenter();
+        break;
+      }
+      case 'I':
+      { // frente direita
+        motorForward();
+        directionRight();
+
+        Serial.println("Forward Right");
+        break;
+      }
+      case 'G':
+      { // frente esquerda
+        motorForward();
+        directionLeft();
+
+        Serial.println("Forward Left");
+        break;
+      }
+      case 'R':
+      { // direita
+        motorStop();
+        directionRight();
+        break;
+      }
+      case 'L':
+      { // esquerda
+        motorStop();
+        directionLeft();
+        break;
+      }
+      case 'B':
+      { // ré
+        motorBack();
+        directionCenter();
+        break;
+      }
+      case 'H':
+      { // ré esquerda
+        motorBack();
+        directionLeft();
+
+        Serial.println("Back Left");
+        break;
+      }
+      case 'J':
+      { // ré direita
+        motorBack();
+        directionRight();
+
+        Serial.println("Back Right");
+        break;
+      }
+      case 'W':
+        Serial.println("Front Lights");
+        break;
+      case 'w':
+        Serial.println("Front Lights Off");
+        break;
+      case 'U':
+        Serial.println("Back Lights On");
+        break;
+      case 'u':
+        Serial.println("Back Lights Off");
+        break;
+      case 'V':
+        play_notes = true;
+        xTaskCreate(taskPlayNotes, "Play Notes", 1024 * 4, NULL, 5, NULL);
+
+        Serial.println("Horn On");
+        break;
+      case 'v':
+        play_notes = false;
+
+        Serial.println("Horn Off");
+        break;
+      case 'X':
+        Serial.println("Extra On");
+        break;
+      case 'x':
+        Serial.println("Extra Off");
+        break;
+      case '0':
+      {
+        speed = 0;
+        Serial.printf("Speed 0: %i\n", speed);
+        break;
+      }
+      case '1':
+      {
+        speed = 120;
+        Serial.printf("Speed 1: %i\n", speed);
+        break;
+      }
+      case '2':
+      {
+        speed = 130;
+        Serial.printf("Speed 2: %i\n", speed);
+        break;
+      }
+      case '3':
+      {
+        speed = 140;
+        Serial.printf("Speed 3: %i\n", speed);
+        break;
+      }
+      case '4':
+      {
+        speed = 160;
+        Serial.printf("Speed 4: %i\n", speed);
+        break;
+      }
+      case '5':
+      {
+        speed = 180;
+        Serial.printf("Speed 5: %i\n", speed);
+        break;
+      }
+      case '6':
+      {
+        speed = 200;
+        Serial.printf("Speed 6: %i\n", speed);
+        break;
+      }
+      case '7':
+      {
+        speed = 210;
+        Serial.printf("Speed 7: %i\n", speed);
+        break;
+      }
+      case '8':
+      {
+        speed = 220;
+        Serial.printf("Speed 8: %i\n", speed);
+        break;
+      }
+      case '9':
+      {
+        speed = 240;
+        Serial.printf("Speed 9: %i\n", speed);
+        break;
+      }
+      case 'q':
+      {
+        speed = 255;
+        Serial.printf("Speed 10: %i\n", speed);
+        break;
+      }
+      case 'D':
+      { // desliga
+        status_connected = false;
+
+        Serial.println("Everything OFF");
+        break;
+      }
+      case 'S':
+      default:
+      {
+        status_connected = true;
+        digitalWrite(CONNECTED_PIN, LOW);
+        directionCenter();
+        motorStop();
+        break;
+      }
+      }
+    }
+
+    if (currentMillis - previousMillisStatus >= interval_status && !status_connected)
+    {
+      previousMillisStatus = currentMillis;
+
+      digitalWrite(CONNECTED_PIN, !digitalRead(CONNECTED_PIN));
+    }
+
+    if (currentMillis - previousMillisBattery >= interval_battery_read && !battery_low)
+    {
+      previousMillisBattery = currentMillis;
+
+      // loopBattery();
+    }
   }
 
   if (currentMillis - previousMillisBatteryLow >= interval_battery_low && battery_low)
